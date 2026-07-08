92b43c0e77a55e90f75927809b22776986c25dec
BillZhou233 <46444349+BillZhou233@users.noreply.github.com>
2026-07-08 11:18:43 +0800

    fish: new, 4.7.1

---
+++ Formula/fish.rb
@@ -0,0 +1,48 @@
+class Fish < Formula
+  desc "User-friendly command-line shell for UNIX-like operating systems"
+  homepage "https://fishshell.com"
+  url "https://github.com/fish-shell/fish-shell/releases/download/4.7.1/fish-4.7.1.tar.xz"
+  sha256 "6f4d5b438a6338e3f5dcda19a28261e2ece7a9b7ff97686685e6abdc31dbb7df"
+  license "GPL-2.0-only"
+  compatibility_version 1
+  head "https://github.com/fish-shell/fish-shell.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  pour_bottle? only_if: :default_prefix
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "72c35917c46817b8ffc7a29a88503dc1c69c480791c00d1153b3c6f080438f24"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+  depends_on "sphinx-doc" => :build
+  depends_on "pcre2"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
+                    "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}",
+                    "-DWITH_DOCS=ON",
+                    "-Dextra_functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d",
+                    "-Dextra_completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d",
+                    "-Dextra_confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  def post_install
+    (pkgshare/"vendor_functions.d").mkpath
+    (pkgshare/"vendor_completions.d").mkpath
+    (pkgshare/"vendor_conf.d").mkpath
+  end
+
+  test do
+    system bin/"fish", "-c", "echo"
+    output = shell_output("#{bin}/fish -c 'set --show fish_function_path'")
+    assert_match "#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d", output
+  end
+end
